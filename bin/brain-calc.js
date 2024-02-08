35009120b12348667cd74aea1047a19c7328cdc4
RaakzBlanvod <icky6119@gmail.com>
2024-02-08 13:28:13 +0300

    step 7 and 8 done

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,45 +1,32 @@
 #!/usr/bin/env node
+import gameBody from '../src/gameBody.js';
 
-import readlineSync from 'readline-sync';
-import nameAsk from '../src/cli.js';
-
-const userName = nameAsk();
+const description = 'What is the result of the expression?';
 
 console.log('What is the result of the expression?');
 
 const task = () => {
-  const num1 = Math.round(Math.random() * 100);
-  const num2 = Math.round(Math.random() * 100);
-  const operators = ['+', '-', '*'];
-  const i = Math.floor(Math.random() * operators.length);
-  let a = 0;
-  if (operators[i] === '+') {
-    a = num1 + num2;
-    return [`${num1} + ${num2}`, a];
-  } if (operators[i] === '-') {
-    a = num1 - num2;
-    return [`${num1} - ${num2}`, a];
+  const operand1 = Math.round(Math.random() * 10);
+  const operand2 = Math.round(Math.random() * 10);
+
+  const operator = ['+', '*', '-'];
+  const indexOper = Math.round(Math.random() * 2);
+
+  let answer = 0;
+
+  switch (indexOper) {
+    case 0:
+      answer = `${operand1 + operand2}`;
+      break;
+    case 1:
+      answer = `${operand1 * operand2}`;
+      break;
+    default:
+      answer = `${operand1 - operand2}`;
+      break;
   }
-  a = num1 * num2;
-  return [`${num1} * ${num2}`, a];
+
+  return [`${operand1} ${operator[indexOper]} ${operand2}`, answer];
 };
 
-let mark = 0;
-
-for (let i = 1; i <= 3; i += 1) {
-  const step = task();
-  console.log(`Question: ${step[0]}`);
-  const answer = readlineSync.question('Answer: ');
-  if (answer == step[1]) {
-    console.log('Correct!');
-    mark += 1;
-  } else {
-    console.log(`${answer} is wrong answer ;(. Correct answer was ${step[1]}`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
-  }
-}
-
-if (mark === 3) {
-  console.log(`Congratulations, ${userName}!`);
-}
+gameBody(description, task);
--- src/gameBody.js
+++ src/gameBody.js
@@ -1,30 +1,30 @@
 import readlineSync from 'readline-sync';
-import greetUser from '../src/cli.js';
+import greetUser from './cli.js';
 
 const gameBody = (description, taskFunc) => {
-    const playerName = greetUser();
-    console.log(description);
+  const playerName = greetUser();
+  console.log(description);
 
-    let mark = 0; 
-    for (let i = 1; i <= 3; i += 1) {
-        const step = taskFunc(); const [q, a] = taskFunc();
+  let mark = 0;
+  for (let i = 1; i <= 3; i += 1) {
+    const step = taskFunc();
 
-        console.log(`Question: ${step[0]}`);
-        const answer = readlineSync.question('Answer: ');
+    console.log(`Question: ${step[0]}`);
+    const answer = readlineSync.question('Answer: ');
 
-        if (answer === step[1]) {
-            console.log('Correct!');
-            mark += 1;
-          } else {
-            console.log(`${answer} is wrong answer ;(. Correct answer was ${step[1]}`);
-            console.log(`Let's try again, ${playerName}!`);
-            break;
-          }
-        }
-        
-        if (mark === 3) {
-          console.log(`Congratulations, ${playerName}!`);
-        }
-    };
+    if (answer === step[1]) {
+      console.log('Correct!');
+      mark += 1;
+    } else {
+      console.log(`${answer} is wrong answer ;(. Correct answer was ${step[1]}`);
+      console.log(`Let's try again, ${playerName}!`);
+      break;
+    }
+  }
 
-    export default gameBody;
+  if (mark === 3) {
+    console.log(`Congratulations, ${playerName}!`);
+  }
+};
+
+export default gameBody;
